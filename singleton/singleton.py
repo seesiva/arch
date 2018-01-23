564ffdf8c6c088d3e86a4f6b780c366ed37643bd
Siva Karthikeyan <seesiva@gmail.com>
2018-01-23 15:32:40 +0530

    Added singleton design pattern

---
+++ singleton/singleton.py
@@ -0,0 +1,4 @@
+"""
+Singleton design pattern is used when we are dealing with an situation where in there is a need for access to only one instance for global access. Typically would be very suitable for handling configuration
+"""
+
